22796d38e8dc67aef3fdfb6a5eafd577605ed9b1
mosisn <mostafa.saneisn@gmail.com>
2024-10-08 22:47:30 +0330

    Chores

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import task_summary
 
 urlpatterns = [
-    path('/task-summary',task_summary, name='Task summary' )
+    path('task-summary/',task_summary, name='task_summary' )
 ]
