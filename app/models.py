ae7bfb3af194b4fa75d3b55dddd8fb90984bf2cf
mosisn <mostafa.saneisn@gmail.com>
2024-10-08 20:31:37 +0330

    Create Task model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('pending','pending'),
+    ('canceled', 'canceled'),
+    ('completed', 'completed')
+)
+
+class Task(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Task Title')
+    description = models.TextField(verbose_name='Task Description', help_text='Provide a detailed description of the task.')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name='Task Status')
+    due_date = models.DateTimeField(verbose_name='Due Date', help_text='Specify the due date and time for the task.')
+    
+    def __str__(self):
+        return self.title
